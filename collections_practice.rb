808db58c7cc1a06bd2e7e0ade0fca33532a10d0d
Saida Davis <saihdavis@gmail.com>
2020-04-05 15:44:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,3 +7,7 @@ end
 def sort_array_char_count(strings)
   strings.sort {|left, right| left.length <=> right.length}
 end
+def swap_elements(strings)
+  strings[1], strings[2] = strings[2], strings[1]
+  return strings
+end
